5e13ba5ed5171af27de1d5ee34a2de9f5476f517
Ilya Kuzmichev <deadbabe@gmail.com>
2016-09-03 01:11:23 +0300

    release preparations

--- make/HotMemMake.hx
+++ make/HotMemMake.hx
@@ -1,5 +1,3 @@
-import hxmake.cli.CL;
-import hxmake.Task;
 import hxmake.test.TestTask;
 import hxmake.haxelib.HaxelibExt;
 import hxmake.idea.IdeaPlugin;
@@ -21,14 +19,14 @@ class HotMemMake extends hxmake.Module {
 
 		library(function(ext:HaxelibExt) {
 			ext.config.version = "0.0.2";
-			ext.config.description = "Hot-memory library for Haxe";
+			ext.config.description = "Hot memory access library for Haxe";
 			ext.config.url = "https://github.com/eliasku/hotmem";
-			ext.config.tags = ["memory", "hot", "data", "access", "primitive", "cross"];
+			ext.config.tags = ["memory", "hot", "data", "array", "access", "primitive", "cross", "cache"];
 			ext.config.contributors = ["eliasku"];
 			ext.config.license = "MIT";
-			ext.config.releasenote = "Initial release";
+			ext.config.releasenote = "check bounds";
 
-			ext.pack.includes = ["src", "haxelib.json", "README.md"];
+			ext.pack.includes = ["src", "haxelib.json", "README.md", "LICENSE"];
 		});
 
 		var tt = new TestTask();
